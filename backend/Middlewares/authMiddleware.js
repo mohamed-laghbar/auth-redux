80bf2202f20a80436b57ae014cc79fe0e7617c34
mohamed laghbar <mohamedlaghbar@MacBook-Pro-de-mohamed.local>
2022-10-31 15:48:03 +0100

    install tailwind

---
+++ backend/Middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const protect = async (req, res, next) => {
+  const token = req.cookies.token;
+  try {
+    if (!token) {
+      return res.status(401).json('You need to Login')
+    }
+    const decrypt = await jwt.verify(token, process.env.JWT_SECRET);
+    req.user = {
+      id: decrypt.id,
+    };
+    next();
+  } catch (err) {
+    return res.status(500).json(err.toString());
+  }
+}
+
+
+
+module.exports = { protect, }
---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>hdhdh</h1>
+ );
+}
+
+export default App;
